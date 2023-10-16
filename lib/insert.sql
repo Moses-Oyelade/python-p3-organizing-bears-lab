fa554dc71760e8cb4f0f7e6f257fd948e82a0245
Moses-Oyelade <oyelademoses@gmail.com>
2023-10-16 15:57:33 +0100

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,22 @@
+-- bearsToInsert = [
+--     ('Mr. Chocoloate', 4, 'M', 'Brown', 'Aggressive', 1),
+--     ('Rowdy', 4, 'M', 'White', 'Aggressive', 1),
+--     ('Tabitha', 2, 'F', 'Black', 'Calm', 0),
+--     ('Sergeant Brown', 5, 'M', 'Brown', 'Aggressive', 1),
+--     ('Melissa', 5, 'M', 'White', 'Friendly', 1),
+--     ('Grinch', 3, 'M', 'Brown', 'Calm', 1),
+--     ('Wendy', 6, 'F', 'White', 'Aggressive', 1),
+--     (NULL, NULL, 'F', 'Pinky', 'NULL', 1)
+-- ]
+
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES
+    ("Mr. Chocoloate", 4, "M", "Brown", "Aggressive", 1),
+    ("Rowdy", 4, "M", "White", "Aggressive", 1),
+    ("Tabitha", 2, "F", "Black", "Calm", 0),
+    ("Sergeant Brown", 5, "M", "Brown", "Aggressive", 1),
+    ("Melissa", 5, "M", "White", "Friendly", 1),
+    ("Grinch", 3, "M", "Brown", "Calm", 1),
+    ("Wendy", 6, "F", "White", "Aggressive", 1),
+    (NULL, NULL, "F", "Pinky", "Clingy", 1);
